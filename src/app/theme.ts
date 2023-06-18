42cb8de2a8c2d4ed000a153159758cf43d16f02d
Hoda ka <hodakakhki@Sinansoft.com>
2023-06-19 00:14:18 +0330

    set up

---
+++ src/app/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const colors = {
+  brand: {
+    primary: "()",
+    primaryLight: "()",
+    primaryDark: "()",
+  },
+};
+export const theme = extendTheme({ colors });
